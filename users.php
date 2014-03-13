90367dafc2910798a3695d6f3f3a52f21a2b7219
vincent90 <scootersbooms932@hotmail.com>
2014-03-13 16:52:31 -0400

    Ajout modifyUser

--- users.php
+++ users.php
@@ -27,6 +27,30 @@ function AddUser($CompanyId, $FirstName, $LastName, $Province, $Password, $Role,
 		print_r ($result->errorInfo()); //remove surement
 	}
 }
+//Permet de modifier un usager, on modifie tout sauf le UserId et le Password
+//ModifyUser('16','1','Vincent','Matte','QC','1','0.23','FR','loassssassl@lggsshhoggl.com');
+function ModifyUser($userId,$CompanyId, $FirstName, $LastName, $Province, $Role, $KmRate, $Language, $Email){
+	include('conn.php'); //remove surement
+	session_start(); //remove surement
+	try{
+		$result = $conn->prepare("UPDATE users SET CompanyId= :CompanyId, FirstName= :FirstName, LastName= :LastName, Province= :Province, Role= :Role, KmRate= :KmRate, Language= :Language, Email= :Email WHERE UserId= :userId");
+		$result->bindValue(':CompanyId',$CompanyId);
+		$result->bindParam(':FirstName',$FirstName);
+		$result->bindParam(':LastName',$LastName);
+		$result->bindParam(':Province',$Province);
+		$result->bindParam(':Role',$Role);
+		$result->bindParam(':KmRate',$KmRate);
+		$result->bindParam(':Language',$Language);
+		$result->bindParam(':Email',$Email);
+		$result->bindParam(':userId',$userId);
+		$result->execute();	
+
+	} catch(Exception $e){
+		die($e);
+		print_r ($result->errorInfo()); //remove surement
+	}
+}
+
 
 
 
