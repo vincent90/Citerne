03acfd887ee4a92af4f14090de799c0ed8b24608
Alexandre Vanier <vanier.alexandre@gmail.com>
2014-03-13 13:28:43 -0400

    Nouveau Bootstrap et ajout du style login

--- login.php
+++ login.php
@@ -11,11 +11,8 @@
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="signin.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <!-- Custom styles -->
+    <link href="css/style.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
