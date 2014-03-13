a08326bbd2a24a37c0d2092b923ad069bbe0fd12
vincent90 <scootersbooms932@hotmail.com>
2014-03-13 13:44:38 -0400

    changement des texts francais

--- lang/lang.php
+++ lang/lang.php
@@ -1,12 +1,10 @@
 <?PHP
-	//Have seperate language files for each language I add, this would be english file
 	function lang($phrase){
-		
 		static $lang = array(
-			'SIGNIN_TITLE' => 'No photo\'s available',
-			'SIGNIN_USERNAME' => 'This user is new'
-			'SIGNIN_PASSWORD' => 'This user is new'
-			'SIGNIN_CONNECTION' => 'This user is new'
+			'SIGNIN_TITLE' => 'Veuillez vous identifier',
+			'SIGNIN_USERNAME' => 'Nom d\'usager',
+			'SIGNIN_PASSWORD' => 'Mot de passe',
+			'SIGNIN_CONNECTION' => 'Se connecter'
 		);
 		return $lang[$phrase];
 	}
